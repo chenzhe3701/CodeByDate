a9375ee34a6e51b717e45e2ea6e1906d14ecf39c
Zhe Chen <chenzhezju@gmail.com>
2020-10-29 12:26:57 -0400

    code_2020_10_25 process the compression-tension data for Mg4Al_C1, a symmetrically cut sample with more controlled microstructure.

---
+++ code_2020_10_25_Mg4Al_C1_curve.m
@@ -0,0 +1,154 @@
+% Analyze compression data for Mg4Al_C1, tested on 2020-10-23
+close all;
+clc;
+
+cd('E:\zhec umich Drive\2020-10-23 Mg4Al_C1 insitu curve');
+
+filename = '2020-10-23 Mg4Al comp_ten_data.lvm';
+delimiterIn = '\t';
+headerlinesIn = 1;
+A = importdata(filename, delimiterIn, headerlinesIn);
+display(A.colheaders);
+disp('size of A.data:')
+disp(size(A.data,1));
+
+filename_2 = '2020-10-23 Mg4Al comp_ten_final_to_0N.lvm';
+B = importdata(filename_2, delimiterIn, headerlinesIn);
+B.data(:,1) = B.data(:,1) + A.data(end,1) + 1;  % remake B time
+
+% [~, ind] = max(A.data(:,3));
+% A.data = A.data(1:ind,:);
+A.data = [A.data; B. data]; % combine B.data into A.data
+A.data(:,6) = A.data(:,6) - A.data(1,6);    % zero strain
+%% make variable
+time = A.data(:,1);
+displacement =  A.data(:,2);
+force =  A.data(:,3);
+motor_speed = A.data(:,4);
+motor_current = A.data(:,5);
+strain =  A.data(:,6)/1000000;
+stress = force/3.48/2.69;   % Mg4Al tested on 2020-10-13
+
+%% explore
+close all;
+
+figure;
+plot(time, displacement, '-r', 'linewidth', 3);
+xlabel('time (sec)');
+ylabel('displacement (mm)');
+set(gca,'fontsize',16);
+
+figure;
+plot(time, force, '-r', 'linewidth', 3);
+xlabel('time (sec)');
+ylabel('load (N)');
+set(gca,'fontsize',16);
+
+figure;
+plot(time, strain, '-r', 'linewidth', 3);
+xlabel('time (sec)');
+ylabel('strain');
+set(gca,'fontsize',16);
+
+%% Select active loading part, exploratory
+disp('Select loading parts:');
+figure;
+plot(time, motor_speed);
+indn_max = length(motor_speed);
+ind = 1;
+while ind < indn_max
+    % find starting point of a segment with motor speed > 0.002
+    if abs(motor_speed(ind)) < 0.003
+        ind = ind + 1;
+    else
+        ind_a = ind;    % starting point
+
+        % find ending point of this segment
+        while (ind < indn_max) && (abs(motor_speed(ind)) > 0.003)
+            ind_b = ind;    % ending point
+            ind = ind + 1;
+        end
+        disp([ind_a, ind_b]);
+    end
+end
+%% Construct active loading part
+data = [];
+ind_stop = 1;
+% load 0 -> 1
+data = [data; A.data(5:781,:)];
+ind_stop = [ind_stop, size(data,1)];
+% load 1 -> 2
+data = [data; A.data(10809:10958,:)];
+ind_stop = [ind_stop, size(data,1)];
+% load 2 -> 3
+data = [data; A.data(21122:21309,:)];
+ind_stop = [ind_stop, size(data,1)];
+% load 3 -> 4
+data = [data; A.data(32195:32567,:)];
+ind_stop = [ind_stop, size(data,1)];
+
+% load 4 -> 5
+data = [data; A.data(42738:44288,:)];
+ind_stop = [ind_stop, size(data,1)];
+% load 5 -> 6
+data = [data; A.data(54502:55452,:)];
+ind_stop = [ind_stop, size(data,1)];
+% load 6 -> 7
+data = [data; A.data(65108:65692,:)];
+ind_stop = [ind_stop, size(data,1)];
+% load 7 -> 8
+data = [data; A.data(65726:66207,:)];
+ind_stop = [ind_stop, size(data,1)];
+
+%% make variable, new, and subplot [disp,force,strain] vs. [time]
+time = data(:,1);
+displacement =  data(:,2);
+force =  data(:,3);
+motor_speed = data(:,4);
+motor_current = data(:,5);
+strain =  data(:,6)/1000000;
+stress = force/3.3/2.74;   
+
+figure;
+subplot(3,1,1); hold on;
+plot(displacement, '-r', 'linewidth', 3);
+plot(ind_stop(1:end-1), displacement(ind_stop(1:end-1)), '.b', 'markersize',16);
+xlabel('time (sec)');
+ylabel('displacement (mm)');
+set(gca,'fontsize',12);
+
+subplot(3,1,2); hold on;
+plot(force, '-r', 'linewidth', 3);
+plot(ind_stop(1:end-1), force(ind_stop(1:end-1)), '.b', 'markersize',16);
+xlabel('time (sec)');
+ylabel('load (N)');
+set(gca,'fontsize',12);
+
+subplot(3,1,3); hold on;
+plot(strain, '-r', 'linewidth', 3);
+plot(ind_stop(1:end-1), strain(ind_stop(1:end-1)), '.b', 'markersize',16);
+xlabel('time (sec)');
+ylabel('strain');
+set(gca,'fontsize',12);
+
+disp('stress at load steps:');
+disp(stress(ind_stop));
+disp('strain at load steps:');
+disp(strain(ind_stop));
+disp('displacement at load steps:');
+disp(displacement(ind_stop));
+%% stress vs strain,  displacement vs strain
+figure; hold on;
+plot(strain, stress, 'linewidth', 3);
+plot(strain(ind_stop(1:end-1)), stress(ind_stop(1:end-1)), 'r.', 'markersize', 18);
+xlabel('Strain, from strain gage');
+ylabel('Stress (MPa)');
+set(gca, 'xlim',[-0.028, 0.001], 'ylim',[-150,150], 'fontsize',18);
+
+figure; hold on;
+plot(displacement, stress, 'linewidth', 3);
+plot(displacement(ind_stop(1:end-1)), stress(ind_stop(1:end-1)), 'r.', 'markersize', 18);
+xlabel('Displacement (mm)');
+ylabel('Stress (MPa)');
+set(gca, 'xlim',[-1.5, 1.5], 'ylim',[-150,150], 'fontsize',18);
+
