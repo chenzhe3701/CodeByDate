09ed4b2925084a9073258921af55060f17885a51
Zhe Chen <chenzhezju@gmail.com>
2021-01-21 15:00:22 -0500

    code_2020_12_29 and 2021_01_17 to process insitu EBSD curve on Mg4Al_C3 and UM134_Mg_C2

---
+++ code_2020_12_29_Mg4Al_C3_curve.m
@@ -0,0 +1,190 @@
+% Analyze compression data for UM_134_Mg_C1, tested on 2020-12-05
+%% load data
+
+close all;
+clc;
+
+working_dir = 'E:\zhec umich Drive\2020-12-23 Mg4Al_C3 insitu curve';
+cd(working_dir);
+
+fileName = '2020-12-23 Mg4Al_C3 comp_ten_data.lvm';
+delimiterIn = '\t';
+headerlinesIn = 1;
+A = importdata(fullfile(working_dir,fileName), delimiterIn, headerlinesIn);
+display(A);
+
+width = 3.47;  % Mg4Al_C3 tested on 2020-12-23
+thickness = 2.64;
+%% make variable
+time = A.data(:,1);
+displacement =  A.data(:,2);
+force =  A.data(:,3);
+motor_speed = A.data(:,4);
+motor_current = A.data(:,5);
+strain =  A.data(:,6)/1000000;
+stress = force/width/thickness;   
+
+%% explore
+close all;
+
+figure;
+plot(time, displacement, '-r', 'linewidth', 3);
+xlabel('time (sec)');
+ylabel('displacement (mm)');
+set(gca,'fontsize',16);
+
+figure;
+plot(time, force, '-r', 'linewidth', 3);
+xlabel('time (sec)');
+ylabel('load (N)');
+set(gca,'fontsize',16);
+
+figure;
+plot(time, strain, '-r', 'linewidth', 3);
+xlabel('time (sec)');
+ylabel('strain');
+set(gca,'fontsize',16);
+
+%% Select active loading part, exploratory
+disp('Select loading parts:');
+figure;
+plot(time, motor_speed);
+indn_max = length(motor_speed);
+ind = 1;
+while ind < indn_max
+    % find starting point of a segment with motor speed > 0.002
+    if abs(motor_speed(ind)) < 0.003
+        ind = ind + 1;
+    else
+        ind_a = ind;    % starting point
+
+        % find ending point of this segment
+        while (ind < indn_max) && (abs(motor_speed(ind)) > 0.003)
+            ind_b = ind;    % ending point
+            ind = ind + 1;
+        end
+        disp([ind_a, ind_b]);
+    end
+end
+
+%% Construct active loading part
+data = [];
+ind_stop = 1;
+% load 0 -> 1
+data = [data; A.data(4:740,:)];
+ind_stop = [ind_stop, size(data,1)];
+% load 1 -> 2
+data = [data; A.data(10795:10992,:)];
+ind_stop = [ind_stop, size(data,1)];
+% load 2 -> 3
+data = [data; A.data(21206:21478,:)];
+ind_stop = [ind_stop, size(data,1)];
+% load 3 -> 4
+data = [data; A.data(31270:31421,:)];
+ind_stop = [ind_stop, size(data,1)];
+% load 4 -> 5
+data = [data; A.data(41242:43007,:)];
+ind_stop = [ind_stop, size(data,1)];
+% load 5 -> 6
+data = [data; A.data(52798:53159,:)];
+ind_stop = [ind_stop, size(data,1)];
+% load 6 -> 7
+data = [data; A.data(68899:69219,:)];
+ind_stop = [ind_stop, size(data,1)];
+% load 7 -> 8
+data = [data; A.data(79018:81262,:)];
+ind_stop = [ind_stop, size(data,1)];
+% load 8 -> 9
+data = [data; A.data(91744:91966,:)];
+ind_stop = [ind_stop, size(data,1)];
+% load 9 -> 10
+data = [data; A.data(101777:102062,:)];
+ind_stop = [ind_stop, size(data,1)];
+% load 10 -> 11
+data = [data; A.data(113578:115844,:)];
+ind_stop = [ind_stop, size(data,1)];
+% load 11 -> 12
+data = [data; A.data(125821:126133,:)];
+ind_stop = [ind_stop, size(data,1)];
+% load 12 -> 13
+data = [data; A.data(135995:136257,:)];
+ind_stop = [ind_stop, size(data,1)];
+% load 13 -> 14
+data = [data; A.data(146336:146694,:)];
+ind_stop = [ind_stop, size(data,1)];
+%% make variable, new, and subplot [disp,force,strain] vs. [time]
+time = data(:,1);
+displacement =  data(:,2);
+force =  data(:,3);
+motor_speed = data(:,4);
+motor_current = data(:,5);
+strain =  data(:,6)/1000000;
+stress = force/width/thickness;
+
+figure; set(gcf,'Position', [150, 150, 1024, 768]);
+subplot(3,1,1); hold on;
+plot(displacement, '-r', 'linewidth', 3);
+plot(ind_stop(1:end-1), displacement(ind_stop(1:end-1)), '.b', 'markersize',16);
+xlabel('time (sec)');
+ylabel('displacement (mm)');
+set(gca,'fontsize',12);
+
+subplot(3,1,2); hold on;
+plot(force, '-r', 'linewidth', 3);
+plot(ind_stop(1:end-1), force(ind_stop(1:end-1)), '.b', 'markersize',16);
+xlabel('time (sec)');
+ylabel('load (N)');
+set(gca,'fontsize',12);
+
+subplot(3,1,3); hold on;
+plot(strain, '-r', 'linewidth', 3);
+plot(ind_stop(1:end-1), strain(ind_stop(1:end-1)), '.b', 'markersize',16);
+xlabel('time (sec)');
+ylabel('strain');
+set(gca,'fontsize',12);
+
+disp('stress at load steps:');
+disp(stress(ind_stop));
+disp('strain at load steps:');
+disp(strain(ind_stop));
+disp('displacement at load steps:');
+disp(displacement(ind_stop));
+
+%% stress vs strain,  displacement vs strain
+figure; hold on;
+% plot(strain, stress, 'linewidth', 3);
+colors = parula(length(ind_stop));
+for ii = 1:length(ind_stop)-1
+    plot(strain(ind_stop(ii):ind_stop(ii+1)), stress(ind_stop(ii):ind_stop(ii+1)), 'color',colors(ii,:), 'linewidth',3);
+end
+plot(strain(ind_stop(1:end-1)), stress(ind_stop(1:end-1)), 'r.', 'markersize', 18);
+xlabel('Strain, from strain gage');
+ylabel('Stress (MPa)');
+set(gca, 'xlim',[-0.03, 0.005], 'ylim',[-150,150], 'fontsize',18);
+
+figure; hold on;
+for ii = 1:length(ind_stop)-1
+    plot(displacement(ind_stop(ii):ind_stop(ii+1)), stress(ind_stop(ii):ind_stop(ii+1)), 'color',colors(ii,:), 'linewidth',3);
+end
+plot(displacement(ind_stop(1:end-1)), stress(ind_stop(1:end-1)), 'r.', 'markersize', 18);
+xlabel('Displacement (mm)');
+ylabel('Stress (MPa)');
+set(gca, 'xlim',[-1.5, 1.5], 'ylim',[-150,150], 'fontsize',18);
+
+
+%% Then, the strain was estimated from EBSD map, and corrected.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ code_2021_01_17_UM134_Mg_C2_curve.m
@@ -0,0 +1,201 @@
+%% load data
+
+close all;
+clc;
+
+working_dir = 'E:\zhec umich Drive\2021-01-15 UM_134 Mg_C2 insitu curve';
+cd(working_dir);
+
+fileName = '2021-01-15 UM134_Mg_C2 comp-ten-data.lvm';
+delimiterIn = '\t';
+headerlinesIn = 1;
+A = importdata(fullfile(working_dir,fileName), delimiterIn, headerlinesIn);
+display(A);
+
+width = 3.44;       % UM134_Mg_C2, tested on 2021-01-15
+thickness = 2.72;
+%% make variable
+time = A.data(:,1);
+displacement =  A.data(:,2);
+force =  A.data(:,3);
+motor_speed = A.data(:,4);
+motor_current = A.data(:,5);
+strain =  A.data(:,6)/1000000;
+stress = force/width/thickness;   
+
+%% explore
+close all;
+
+figure;
+plot(time, displacement, '-r', 'linewidth', 3);
+xlabel('time (sec)');
+ylabel('displacement (mm)');
+set(gca,'fontsize',16);
+
+figure;
+plot(time, force, '-r', 'linewidth', 3);
+xlabel('time (sec)');
+ylabel('load (N)');
+set(gca,'fontsize',16);
+
+figure;
+plot(time, strain, '-r', 'linewidth', 3);
+xlabel('time (sec)');
+ylabel('strain');
+set(gca,'fontsize',16);
+
+%% Select active loading part, exploratory
+disp('Select loading parts:');
+figure;
+plot(time, motor_speed);
+indn_max = length(motor_speed);
+ind = 1;
+while ind < indn_max
+    % find starting point of a segment with motor speed > 0.002
+    if abs(motor_speed(ind)) < 0.003
+        ind = ind + 1;
+    else
+        ind_a = ind;    % starting point
+
+        % find ending point of this segment
+        while (ind < indn_max) && (abs(motor_speed(ind)) > 0.003)
+            ind_b = ind;    % ending point
+            ind = ind + 1;
+        end
+        disp([num2str(ind_a),',',num2str(ind_b),';']);
+    end
+end
+
+%% For this experiment, the displacement reading did not change well during test, so we need to manually fix
+% for active loading part, the rate is 1 um/s
+% for other parts, the rate is 0 um/s
+inds = [4,1204;
+10803,10908;
+20403,20565;
+30553,30876;
+40404,41788;
+51303,51522;
+61004,61180;
+72603,74447;
+84903,85049;
+94603,94782;
+104303,106147;
+115703,115889;
+125353,125513;
+135020,135118];
+
+sign_v = [-1,-1,-1,1,1,1,1,-1,-1,-1,1,1,1,-1]; % increase or decrease displacement  
+disp_v = zeros(size(A.data,1),1);
+for ir = 1:size(inds,1)
+   disp_local = (1:(inds(ir,2)-inds(ir,1)+1)) * sign_v(ir);   % calculate local series
+   disp_v(inds(ir,1):inds(ir,2)) = disp_v(inds(ir,1)) + disp_local; % add local increase
+   disp_v(inds(ir,2):end) = disp_v(inds(ir,2));     % from pos_index_2, copy the value at pos_index_2
+end
+
+A.data(:,2) = disp_v / 1000;
+%% Construct active loading part
+data = [];
+ind_stop = 1;
+% load 0 -> 1
+data = [data; A.data(4:1204,:)];
+ind_stop = [ind_stop, size(data,1)];
+% load 1 -> 2
+data = [data; A.data(10803:10908,:)];
+ind_stop = [ind_stop, size(data,1)];
+% load 2 -> 3
+data = [data; A.data(20403:20565,:)];
+ind_stop = [ind_stop, size(data,1)];
+% load 3 -> 4
+data = [data; A.data(30553:30876,:)];
+ind_stop = [ind_stop, size(data,1)];
+% load 4 -> 5
+data = [data; A.data(40404:41788,:)];
+ind_stop = [ind_stop, size(data,1)];
+% load 5 -> 6
+data = [data; A.data(51303:51522,:)];
+ind_stop = [ind_stop, size(data,1)];
+% load 6 -> 7
+data = [data; A.data(61004:61180,:)];
+ind_stop = [ind_stop, size(data,1)];
+% load 7 -> 8
+data = [data; A.data(72603:74447,:)];
+ind_stop = [ind_stop, size(data,1)];
+% load 8 -> 9
+data = [data; A.data(84903:85049,:)];
+ind_stop = [ind_stop, size(data,1)];
+% load 9 -> 10
+data = [data; A.data(94603:94782,:)];
+ind_stop = [ind_stop, size(data,1)];
+% load 10 -> 11
+data = [data; A.data(104303:106147,:)];
+ind_stop = [ind_stop, size(data,1)];
+% load 11 -> 12
+data = [data; A.data(115703:115889,:)];
+ind_stop = [ind_stop, size(data,1)];
+% load 12 -> 13
+data = [data; A.data(125353:125513,:)];
+ind_stop = [ind_stop, size(data,1)];
+% load 13 -> 14
+data = [data; A.data(135020:135118,:)];
+ind_stop = [ind_stop, size(data,1)];
+
+%% make variable, new, and subplot [disp,force,strain] vs. [time]
+time = data(:,1);
+displacement =  data(:,2);
+force =  data(:,3);
+motor_speed = data(:,4);
+motor_current = data(:,5);
+strain =  data(:,6)/1000000;
+stress = force/width/thickness;   
+
+figure; set(gcf,'Position', [150, 150, 600, 800]);
+subplot(3,1,1); hold on;
+plot(displacement, '-r', 'linewidth', 3);
+plot(ind_stop(1:end-1), displacement(ind_stop(1:end-1)), '.b', 'markersize',16);
+xlabel('time (sec)');
+ylabel('displacement (mm)');
+set(gca,'fontsize',12,'xlim',[0 9000]);
+
+subplot(3,1,2); hold on;
+plot(force, '-r', 'linewidth', 3);
+plot(ind_stop(1:end-1), force(ind_stop(1:end-1)), '.b', 'markersize',16);
+xlabel('time (sec)');
+ylabel('load (N)');
+set(gca,'fontsize',12,'xlim',[0 9000]);
+
+subplot(3,1,3); hold on;
+plot(strain, '-r', 'linewidth', 3);
+plot(ind_stop(1:end-1), strain(ind_stop(1:end-1)), '.b', 'markersize',16);
+xlabel('time (sec)');
+ylabel('strain');
+set(gca,'fontsize',12,'xlim',[0 9000]);
+
+disp('stress at load steps:');
+disp(stress(ind_stop));
+disp('strain at load steps:');
+disp(strain(ind_stop));
+disp('displacement at load steps:');
+disp(displacement(ind_stop));
+
+%% stress vs strain,  displacement vs strain
+figure; hold on;
+% plot(strain, stress, 'linewidth', 3);
+colors = parula(length(ind_stop));
+for ii = 1:length(ind_stop)-1
+    plot(strain(ind_stop(ii):ind_stop(ii+1)), stress(ind_stop(ii):ind_stop(ii+1)), 'color',colors(ii,:), 'linewidth',3);
+end
+plot(strain(ind_stop(1:end-1)), stress(ind_stop(1:end-1)), 'r.', 'markersize', 18);
+xlabel('Strain, from strain gage');
+ylabel('Stress (MPa)');
+set(gca, 'xlim',[-0.03, 0.005], 'ylim',[-75,75], 'fontsize',18);
+
+figure; hold on;
+for ii = 1:length(ind_stop)-1
+    plot(displacement(ind_stop(ii):ind_stop(ii+1)), stress(ind_stop(ii):ind_stop(ii+1)), 'color',colors(ii,:), 'linewidth',3);
+end
+plot(displacement(ind_stop(1:end-1)), stress(ind_stop(1:end-1)), 'r.', 'markersize', 18);
+xlabel('Displacement (mm)');
+ylabel('Stress (MPa)');
+set(gca, 'xlim',[-2, 1], 'ylim',[-75,75], 'fontsize',18);
+
+
