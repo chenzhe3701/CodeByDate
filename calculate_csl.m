61120f4ff23a410be5fb69c60e974c6655622686
Zhe Chen <chenzhezju@gmail.com>
2021-10-17 22:40:05 -0400

    code_2021_10_17 try clean pixel-wise variant map by connected segment

---
+++ calculate_csl.m
@@ -0,0 +1,78 @@
+function csl_out = calculate_csl(twin_map, active_variants, trace_dir)
+
+for iTwin = 1:6
+    if active_variants(iTwin)>0
+        map_r = imrotate(twin_map, trace_dir(iTwin), 'nearest', 'loose');   % rotated map
+        [nr,nc] = size(map_r);
+        label_map = zeros(nr,nc);    % to store assigned temporary label
+        csl_map = zeros(nr,nc);
+        for ir = 1:nr
+            if any(map_r(ir,:))
+                icL_back = find(map_r(ir,:),1,'first');
+                icR_back = find(map_r(ir,:),1,'last');
+                icL_front = find(map_r(ir,:),1,'first');
+                icR_front = find(map_r(ir,:),1,'last');
+                while (icL_front<=icR_front)
+                    csl_length = 0;
+                    if (icL_front-icL_back)<=(icR_back-icR_front)
+                        % search for connected segments from left to right
+                        while(map_r(ir,icL_front))
+                            label_map(ir,icL_front) = 1;  % prepare to assign label
+                            map_r(ir,icL_front) = 0;     % make element on map_r 0 (temp change)
+                            icL_front = icL_front + 1;   % move pointer forward to the right
+                            csl_length = csl_length + 1;
+                        end
+                        csl_map(label_map==1) = csl_length;
+                        label_map(label_map==1) = 0;     % change back (equivalent to change to gb label)
+                        icL_back = icL_front - 1;    % assign left side back
+                        icL_front = find(map_r(ir,:),1,'first'); % search left side front again
+                    else
+                        while (map_r(ir,icR_front))
+                            label_map(ir,icR_front) = 1;
+                            map_r(ir,icR_front) = 0;
+                            icR_front = icR_front - 1;
+                            csl_length = csl_length + 1;
+                        end
+                        csl_map(label_map==1) = csl_length;
+                        label_map(label_map==1) = 0;
+                        icR_back = icR_front + 1;
+                        icR_front = find(map_r(ir,:),1,'last');
+                    end
+                end
+            end
+        end
+        
+        temp = imrotate(csl_map, -trace_dir(iTwin), 'nearest', 'loose');
+        img1_template = (twin_map>0);
+        img2_signal = (temp>0);
+        [yOffSet, xOffSet] = normxcorr2A_register(img1_template, img2_signal, [0 0 0 0], [0 0 0 0], 0);
+        indC_back_min = 1 + xOffSet;
+        indC_back_max = indC_back_min + size(twin_map,2)-1;
+        indR_back_min = 1 + yOffSet;
+        indR_back_max = indR_back_min + size(twin_map,1)-1;
+        
+        csl_p(:,:,iTwin) = temp(indR_back_min:indR_back_max, indC_back_min:indC_back_max);
+    end
+end
+
+[nr,nc,np] = size(csl_p);
+csl = zeros(nr,nc);   % variant number map of the pixels need to analyze
+for ir=1:nr
+    for ic=1:nc
+        [max_value, vN] = max(csl_p(ir,ic,:));
+        if max_value>0
+            csl(ir,ic) = vN;
+        end
+    end
+end
+
+% remove those with twin_map==0
+csl(twin_map==0) = 0;
+% myplot(csl);
+csl_out = one_pass_fill(csl, 0.01);
+myplot(csl_out);
+if sum(csl_out(twin_map(:)==0))
+   error('need check, did not clean enough') 
+end
+end
+
---
+++ code_2021_10_17_try_clean_variant_with_csl.m
@@ -0,0 +1,223 @@
+% Use Mg4Al_U2, iE=3, grain #75, to study if there is a better voting
+% method to determine twin variant?
+%
+% This seems to be a very good example, because
+% (1) The child grain has two twin variants, need to seperate
+% (2) The orientation has spread, so in the twin area of one variant, the
+% pixel-level identification has different results of two variants.
+%
+% If one 'child grain' contains >1 variants, we should do further analysis 
+% 
+% This code is used to study the possibility of using the 'connected
+% segment length' map to do a further clean up.
+
+%% copy setup
+clear; clc; close all;
+addChenFunction;
+working_dir = 'E:\zhec umich Drive\2021-02-26 Mg4Al_U2 EBSD';
+save_dir = [working_dir, '\analysis'];
+mkdir(save_dir);
+% cd(working_dir);
+
+sample_name = 'Mg4Al_U2';
+
+%% copy part-5: find out variants
+po_tolerance_angle = 10; % if child grain has misorientation < po_tolerance_angle with undeformed parent grain, it is considered as having parent orientation
+twin_tolerance_angle = 10;  % if child grain has misorientation < twin_tolerance_angle to a potential twin variant, it is identified as that twin variant
+iE = 3
+d = load(fullfile(save_dir, [sample_name,'_parent_grain_file_iE_0.mat']));
+gID_0 = d.gID;
+gPhi1_0 = d.gPhi1;
+gPhi_0 = d.gPhi;
+gPhi2_0 = d.gPhi2;
+ID_0 = d.ID;
+boundary_0 = find_boundary_from_ID_matrix(ID_0);
+
+d = load(fullfile(save_dir, [sample_name,'_parent_grain_file_iE_',num2str(iE),'.mat']));
+x = d.x;
+y = d.y;
+gID_p = d.gID;
+gPhi1_p = d.gPhi1;
+gPhi_p = d.gPhi;
+gPhi2_p = d.gPhi2;
+ID_p = d.ID;
+boundary_p = find_boundary_from_ID_matrix(ID_p);
+
+% data where twins (children) are individially labeled with IDs
+d = load(fullfile(save_dir, [sample_name,'_grain_file_iE_',num2str(iE),'.mat']));
+gID_c = d.gID;
+gPhi1_c = d.gPhi1;
+gPhi_c = d.gPhi;
+gPhi2_c = d.gPhi2;
+ID_c = d.ID;
+phi1_c = d.phi1;
+phi_c = d.phi;
+phi2_c = d.phi2;
+boundary_c = find_boundary_from_ID_matrix(ID_c);
+
+ID_variant_grain_wise = zeros(size(ID_p));
+ID_variant_point_wise = zeros(size(ID_p));
+Misorientation_point_wise = zeros(size(ID_p));
+
+for ii = 68       % 68
+    id_p = gID_p(ii);
+    disp(['current ID = ',num2str(id_p)]);
+    
+    
+    
+    ind_local = ismember(ID_p, id_p); %ismember(ID, [ID_current,ID_neighbor]);
+    
+    % Make it one data point wider on each side
+    indC_min = max(1, find(sum(ind_local, 1), 1, 'first')-1);
+    indC_max = min(size(ID_p,2), find(sum(ind_local, 1), 1, 'last')+1);
+    indR_min = max(1, find(sum(ind_local, 2), 1, 'first')-1);
+    indR_max = min(size(ID_p,1), find(sum(ind_local, 2), 1, 'last')+1);
+    
+    ID_c_local = ID_c(indR_min:indR_max, indC_min:indC_max);
+    x_local = x(indR_min:indR_max, indC_min:indC_max);
+    y_local = y(indR_min:indR_max, indC_min:indC_max);
+    boundary_c_local = boundary_c(indR_min:indR_max, indC_min:indC_max);
+    phi1_c_local = phi1_c(indR_min:indR_max, indC_min:indC_max);
+    phi_c_local = phi_c(indR_min:indR_max, indC_min:indC_max);
+    phi2_c_local = phi2_c(indR_min:indR_max, indC_min:indC_max);
+    
+    vg_local = zeros(size(ID_c_local)); % variant map, grain level, local  
+    vp_local = zeros(size(ID_c_local)); % variant map, pixel level, local
+    active_variants = zeros(6);
+    
+    
+    
+    
+    % Find the parent orientation at iE = 0
+    ind_0 = find(gID_0 == id_p);
+    euler_0 = [gPhi1_0(ind_0), gPhi_0(ind_0), gPhi2_0(ind_0)]
+    
+    if ~isempty(ind_0)
+        % find out all the id numbers on ID_c overlap with the parent grain on ID_p
+        id_c = unique(ID_c(ID_p == id_p));
+        
+        % Additionally, the child should have > 70% of its pixels overlap with the potential parent grain
+        area_c = zeros(size(id_c));
+        overlap_c = zeros(size(id_c));
+        for kk = 1:length(area_c)
+            area_c(kk) = sum(ID_c(:)==id_c(kk));
+            overlap_c(kk) = sum((ID_p(:)==id_p)&(ID_c(:)==id_c(kk)));
+        end
+        overlap_pct_c = overlap_c./area_c;
+        id_c(overlap_pct_c<0.75) = [];
+        
+        % Modify all the child orientation to crystallographically equivalent one that is closest to euler_0
+        misorientation = [];
+        col_ID = [];
+        col_euler = [];
+        for jj = 1:length(id_c)
+            id = id_c(jj);
+            ind = (gID_c == id);
+            euler_id = [gPhi1_c(ind), gPhi_c(ind), gPhi2_c(ind)];
+            
+            % =================> Important, here modified the grain info of the child grain
+            euler_id = find_closest_orientation_hcp(euler_id, euler_0);
+            gPhi1_c(ind) = euler_id(1);
+            gPhi_c(ind) = euler_id(2);
+            gPhi2_c(ind) = euler_id(3);
+            
+            misorientation(jj,1) = calculate_misorientation_euler_d(euler_0, euler_id, 'hcp');
+            col_ID = [col_ID; id];
+            col_euler = [col_euler; euler_id];
+        end
+        tbl = table(col_ID, col_euler, misorientation); % for debugging
+        
+        % (1) Find out which 'children' is actually the 'parent', by checking the misorientation
+        % Note: multiple 'grains' can have the parent orientation
+        inds = find(misorientation < po_tolerance_angle);
+        if ~isempty(inds)
+            % Use the average of all the 'child grains' with parent orientation.
+            id_po = id_c(inds);
+            inds_po = find(ismember(gID_c,id_po));
+            euler_po = calculate_average_dominant_euler_hcp([gPhi1_c(inds_po), gPhi_c(inds_po), gPhi2_c(inds_po)]);
+            
+            % remove the 'child grains' with the 'parent orientation', leave only the true 'twin children'
+            id_c(inds) = [];
+        else
+            % the child maybe fully twinned. Just use euler_0 as euler_po
+            euler_po = euler_0;
+            warning('The grain might have completely twinned');
+            str = sprintf('iE = %d, ii = %d, ID_parent = %d \n', iE, ii, id_p);
+            disp(str);
+        end
+        
+        % (2) Find out if the remaining child grain is a twin
+        for jj = 3 % 1:length(id_c)
+            id = id_c(jj);  % twin grains id
+            ind = (gID_c == id);
+            euler_id = [gPhi1_c(ind), gPhi_c(ind), gPhi2_c(ind)];
+            
+            
+            variant_child_grain = zeros(size(ID_c_local)); % child grain variant map, pixel level, local
+            miso_child_grain = zeros(size(ID_c_local)); 
+            
+            misorientation = [];
+            %  if (demoTF==1)&&(jj==1)&&(ii==ii_demo)
+            %       hcp_cell('euler',euler_id,'material','Mg','plotPlane',0,'plotBurgers',0,'plotTrace',0);
+            %  end
+            
+            % Determine if this child grain is a twin variant:
+            % Compare [euler of this child] vs [euler of the iTwin system of the parent orientation]
+            for kk = 1:6
+                euler_kk = euler_by_twin(euler_po, kk, 'Mg');    % calculate the twin euler angle for twin system kk
+                misorientation(kk) = calculate_misorientation_euler_d(euler_id, euler_kk, 'HCP');
+                % if (demoTF==1)&&(jj==1)&&(ii==ii_demo)
+                % 	hcp_cell('euler',euler_po,'material','Mg','ss',24+kk);
+                % 	hcp_cell('euler',euler_kk,'material','Mg','ss',24+kk,'plotPlane',0,'plotBurgers',0,'plotTrace',0);
+                % end
+            end
+            [min_val, iVariant_child] = min(abs(misorientation));    % find out
+            
+            % ==============> The child grain may be a twin area containing multiple variants. Assume the child orientation represents at least one true twin orientation
+            % Ff small enough, the child grain should be a twin. Do point-wise analysis
+            if min_val < twin_tolerance_angle
+                ID_variant_grain_wise(ID_c == id) = iVariant_child; % grain-wise variant map
+                
+                ind_list_local = find(ID_c_local==id);
+                ind_list = find(ID_c==id);
+                for mm = 1:length(ind_list)
+                    ind = ind_list(mm);
+                    euler_c = [phi1_c(ind), phi_c(ind), phi2_c(ind)];
+                    
+                    misorientation = [];
+                    % compare [euler of this pixel]  vs  [euler of the iTwin system of the parent orientation]
+                    for kk = 1:6
+                        euler_kk = euler_by_twin(euler_po, kk, 'Mg');    % calculate the twin euler angle for twin system kk
+                        misorientation(kk) = calculate_misorientation_euler_d(euler_c, euler_kk, 'HCP');
+                    end
+                    [miso, iVariant] = min(abs(misorientation));
+                    Misorientation_point_wise(ind) = miso;
+                    ID_variant_point_wise(ind) = iVariant;
+                    
+                    variant_child_grain(ind_list_local(mm)) = iVariant; 
+                    miso_child_grain(ind_list_local(mm)) = miso;
+                end
+            else
+                warning('Twin grain misorientation with parent orientation > 10 deg, rejected as a variant:');
+                str = sprintf('iE = %d, ii = %d, ID_parent = %d, jj = %d, ID_twin = %d \n', iE, ii, id_p, jj, id);
+                disp(str);
+            end            
+            active_variants = ismember(1:6, unique(variant_child_grain(:)));
+            
+            % now we have map_to_analyze = variant_child_grain>0, 'active_variants', 'trace_dir'  
+            [abs_schmid_factor, ~, ~] = trace_analysis_TiMgAl(euler_po, [0,0,0], [0,0,0], [-1 0 0; 0 0 0; 0 0 0], 'Mg', 'twin');
+            traceDir = abs_schmid_factor(19:24,3); 
+            map = calculate_csl(variant_child_grain>0, active_variants, traceDir);
+        end
+    end
+end
+myplot(variant_child_grain);
+caxis([0 6]);
+myplot(miso_child_grain);
+myplot(map);
+% variant_grain_wise{iE} = ID_variant_grain_wise;
+% variant_point_wise{iE} = ID_variant_point_wise;
+
+
+
+
