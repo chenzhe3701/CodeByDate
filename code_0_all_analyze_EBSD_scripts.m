5fffb3e06e43f38e399cf423a729134fc4710718
Zhe Chen <chenzhezju@gmail.com>
2021-11-24 12:30:13 -0500

    Create code_0_all_analyze_EBSD_scripts.m

---
+++ code_0_all_analyze_EBSD_scripts.m
@@ -0,0 +1,40 @@
+% all scripts analyze EBSD
+open('code_2021_03_02_Mg4Al_U2_analyze_EBSD.m');    % done
+winopen('E:\zhec umich Drive\2021-02-26 Mg4Al_U2 EBSD');
+%%
+open('code_2021_02_02_Mg4Al_C3_analyze_EBSD.m');    % done
+winopen('E:\zhec umich Drive\2020-12-23 Mg4Al_C3 insitu EBSD');
+%%
+open('code_2021_04_01_Mg4Al_C1_analyze_EBSD.m');    % done
+winopen('E:\zhec umich Drive\2020-10-23 Mg4Al_C1 insitu EBSD');
+
+
+%%
+open('code_2021_11_08_Mg4Al_A1_analyze_EBSD.m');    % done  
+winopen('E:\zhec umich Drive\2021-10-28 Mg4Al_A1 insitu EBSD');
+%%
+open('code_2021_11_09_Mg4Al_A2_analyze_EBSD.m');    % done
+winopen('E:\zhec umich Drive\2021-11-05 Mg4Al_A2 insitu EBSD');
+
+
+%%
+open('code_2021_02_25_UM134_Mg_C3_analyze_EBSD.m'); % done 
+winopen('E:\zhec umich Drive\2021-01-29 UM134 Mg_C3 insitu EBSD');
+%%
+open('code_2021_04_02_UM134_Mg_C1_analyze_EBSD.m'); % done
+winopen('E:\zhec umich Drive\2020-12-05 UM134 Mg_C1 insitu EBSD');
+%%
+open('code_2021_04_02_UM134_Mg_C2_analyze_EBSD.m'); % done
+winopen('E:\zhec umich Drive\2021-01-15 UM134 Mg_C2 insitu EBSD');
+
+
+%%
+open('code_2021_07_14_UM129_Mg_C1_analyze_EBSD.m'); % done
+winopen('E:\zhec umich Drive\2021-06-29 UM129 Mg_C1 insitu EBSD');
+%%
+open('code_2021_08_22_UM129_Mg_C2_analyze_EBSD.m'); % done
+winopen('E:\zhec umich Drive\2021-08-20 UM129 Mg_C2 insitu EBSD');
+%%
+open('code_2021_09_07_UM129_Mg_C3_analyze_EBSD.m'); % done
+winopen('E:\zhec umich Drive\2021-09-03 UM129_Mg_C3 insitu EBSD');
+
