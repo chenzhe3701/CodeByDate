381987557bcadecd19248a0f747ff52073402342
Zhe Chen <chenzhezju@gmail.com>
2021-12-09 19:00:24 -0500

    move codes to form an individual project

--- insitu_EBSD_codes/custom_functions/caxism.m
+++
@@ -1,7 +0,0 @@
-% use this as 'caxis' to set caxis for plot made by 'myplotm'
-
-function [] = caxism(clim)
-    cmap = colormap();
-    step = (clim(2)-clim(1))/(size(cmap,1)-2);
-    caxis(clim + [-step, step]);
-end
